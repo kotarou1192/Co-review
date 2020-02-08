54cdbf5807e9a1715e406b43559cb3ca32fdb93e
nimuC <nimuC@outlook.com>
2020-02-08 10:48:14 +0900

    Add User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_100524) do
+ActiveRecord::Schema.define(version: 2020_02_08_014616) do
 
   create_table "review_requests", force: :cascade do |t|
     t.integer "user_id"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2020_02_04_100524) do
     t.string "tag_name"
     t.integer "request_id"
     t.boolean "is_pinned", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

c92c8400963b712bbc331bc7b9d3254a21814ab7
t1una <totugekiluna@gmail.com>
2020-05-30 22:36:49 +0900

    fix:  user_id migration(change_column => remove_column & add_column) #51

--- db/migrate/20200204100524_change_column_to_review_requests.rb
+++ db/migrate/20200204100524_change_column_to_review_requests.rb
@@ -1,6 +1,7 @@
 class ChangeColumnToReviewRequests < ActiveRecord::Migration[6.0]
   def change
-    change_column :review_requests, :user_id, :integer
+    remove_column :review_requests, :user_id
+    add_column :review_requests, :user_id, :integer    
     add_column :review_requests, :request_status, :string
     add_column :review_requests, :additional_remark, :string
     add_column :review_requests, :is_frozen, :boolean, default: false

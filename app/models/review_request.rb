7286d60c037b8ba95e80d871ba1328b333059e95
takashiii <azufa.fish@gmail.com>
2020-06-16 18:02:35 +0900

    update: add associations

--- app/models/review_request.rb
+++ app/models/review_request.rb
@@ -1,4 +1,5 @@
 class ReviewRequest < ApplicationRecord
+  has_many :tag, dependent: :destroy
   validates :title, presence: true, length: { maximum: 120 }, uniqueness: true
   validates :text, presence: true, length: { maximum: 2000 }, uniqueness: true
 end

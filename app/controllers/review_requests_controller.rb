9d30f3f8e02e8e9eecc008700f46b902a79aa427
takashiii <azufa.fish@gmail.com>
2020-06-18 13:10:43 +0900

    add: Re-implement tag

--- app/controllers/review_requests_controller.rb
+++ app/controllers/review_requests_controller.rb
@@ -1,6 +1,7 @@
 class ReviewRequestsController < ApplicationController
   def show
     @review_request = ReviewRequest.find(params[:id])
+    @tags = Tag.where(review_request_id: @review_request.id)
   end
 
   def index
@@ -8,11 +9,16 @@ class ReviewRequestsController < ApplicationController
   end
 
   def new
+    @tag_name_error_messages ||= []
     @review_request = ReviewRequest.new
+    @tags = Tag::MAX_TAGS_COUNT.times.map { Tag.new }
   end
 
   def create
     @review_request = ReviewRequest.new(review_request_params)
+    @tags = Tag::MAX_TAGS_COUNT.times.map do |num|
+      Tag.new(tag_params[num.to_s])
+    end
     # p '------'
     # p params
     # p '------'
@@ -25,18 +31,35 @@ class ReviewRequestsController < ApplicationController
     # permitted: false>
     # "------"
 
-    return render action: :new unless @review_request.valid?
+    @tag_name_error_messages = Tag.create_tag_error_messages(@tags)
+
+    return render action: :new unless @review_request.valid? && Tag.any_tag_name_length_too_long?(@tags) && Tag.all_tag_names_unique?(@tags)
 
     ActiveRecord::Base.transaction do
       @review_request.save!
+      save_tags(@review_request.id)
     end
     redirect_to @review_request
   end
 
   private
 
+  def save_tags(review_request_id)
+    @tags.each do |tag|
+      next if tag.name.empty?
+
+      tag.review_request_id = review_request_id
+      tag.pinned = true
+      tag.save!
+    end
+  end
+
   def review_request_params
     # for safety, choose only required parameters
     params.require(:review_request).permit(:title, :text)
+  end
+
+  def tag_params
+    params[:tag].permit!.to_h
   end
 end
